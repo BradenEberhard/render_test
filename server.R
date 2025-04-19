4a603458e8b18d5ef2609e677ec39081883a91a7
be939 <50636857+BradenEberhard@users.noreply.github.com>
2025-04-18 18:32:16 -0600

    maybe

--- server.R
+++ server.R
@@ -1,24 +1,24 @@
 
 function(input, output, session) {
-  # mod_player_leaderboard_server("player_leaderboard", all_player_stats)
-  # observeEvent(input$go_home, {
-  #   # Change the active panel to "Home" when the logo is clicked
-  #   updateNavbarPage(session, "page_navbar", selected = "Home")
-  # })
-  # pool <- get_db_pool()
-  # all_player_stats <- reactiveVal(NULL)
-  # loading_stats <- reactiveVal(TRUE) # To track loading state
+  mod_player_leaderboard_server("player_leaderboard", all_player_stats)
+  observeEvent(input$go_home, {
+    # Change the active panel to "Home" when the logo is clicked
+    updateNavbarPage(session, "page_navbar", selected = "Home")
+  })
+  pool <- get_db_pool()
+  all_player_stats <- reactiveVal(NULL)
+  loading_stats <- reactiveVal(TRUE) # To track loading state
 
-  # # Start the database query in the background
-  # future({
-  #   get_all_player_stats(pool)
-  # }) %...>% {
-  #   all_player_stats(.)
-  #   loading_stats(FALSE) # Set loading to false when done
-  # } %...!% {
-  #   # Handle potential errors here
-  #   showNotification(paste("Error loading player stats:", .), type = "error")
-  #   loading_stats(FALSE) # Ensure loading state is updated on error
-  # } 
+  # Start the database query in the background
+  future({
+    get_all_player_stats(pool)
+  }) %...>% {
+    all_player_stats(.)
+    loading_stats(FALSE) # Set loading to false when done
+  } %...!% {
+    # Handle potential errors here
+    showNotification(paste("Error loading player stats:", .), type = "error")
+    loading_stats(FALSE) # Ensure loading state is updated on error
+  } 
   
 }
